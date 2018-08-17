132813a3425e0e5363b8a018b1fa73a304d2a685
Saatvik Shukla <me@saatvikshukla.com>
2018-08-17 14:03:36 +0530

    config.py: Add DevelopmentConfig

--- analyseether/config.py
+++ analyseether/config.py
@@ -3,6 +3,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # enviornment variables are defined in app.yaml
 
 
+class DevelopmentConfig(object):
+    # Configurations for production enviornment
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_PROD_DATABASE_URI']
+    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+
+    # mail settings
+    MAILGUN_DOMAIN = os.environ['MAILGUN_DOMAIN']
+    MAILGUN_API_URL = os.environ['MAILGUN_API_URL']
+    MAILGUN_API_KEY = os.environ['MAILGUN_API_KEY']
+    MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
+
 class ProductionConfig(object):
     # Configurations for production enviornment
     DEBUG = True
